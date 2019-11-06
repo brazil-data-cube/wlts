20c4d027dc4d928057656f93fa3584f2e583cb6c
gqueiroz <gribeiro.mg@gmail.com>
2019-11-06 10:01:22 -0300

    exporting version symbol

--- wlts/__init__.py
+++ wlts/__init__.py
@@ -1,8 +1,20 @@
+#
+# This file is part of Web Land Trajectory Service.
+# Copyright (C) 2019 INPE.
+#
+# Web Land Trajectory Service is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+"""Web Land Trajectory Service."""
+
 import os
 from wlts.config import get_settings
 from wlts.blueprint import blueprint
 from flask import Flask
 from flask_cors import CORS
+
+from .version import __version__
 
 def create_app(config_name):
 
@@ -27,3 +39,5 @@ app = create_app(get_settings(os.environ.get('ENVIRONMENT', 'DevelopmentConfig')
 
 
 CORS(app, resorces={r'/d/*': {"origins": '*'}})
+
+__all__ = ( '__version__', )
