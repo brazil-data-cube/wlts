5c3131c6f874c3b97f07287b2891aafd6eb49bc7
Fabiana Zioti <fabiana_zioti@hotmail.com>
2019-11-19 17:39:25 -0300

    adding Travis CI

---
+++ tests/test_controller.py
@@ -0,0 +1,13 @@
+#
+# This file is part of Web Land Trajectory Service.
+# Copyright (C) 2019 INPE.
+#
+# Web Land Trajectory Service is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+"""Unit-test for WLTS' controller."""
+
+
+def test_list_collections():
+    assert 1 == 1
