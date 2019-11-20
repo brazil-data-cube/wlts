a92b294962a770d3a89b1d7924dc323daf4ba374
Fabiana Zioti <fabiana_zioti@hotmail.com>
2019-11-20 11:28:47 -0300

    improve test suite

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -9,39 +9,20 @@
 """Unit-test for WLTS' controller."""
 
 import  pytest
-import unittest
 from wlts import app as wlts_app
-from wlts.schemas import collections_list_response, describe_collection_response, trajectory_response
-
-from json import loads as json_loads
-from jsonschema import validate
 
 @pytest.fixture
 def app():
-
     app = wlts_app.test_client()
 
     return app
 
 def test_get_list_collections(app):
-
     response = app.get('/wlts/list_collections')
 
     assert 200 == response.status_code
 
-def  test_list_collections_response(app):
-
+def test_list_collections_response(app):
     response = app.get('/wlts/list_collections')
 
     assert response.content_type == 'application/json'
-
-
-#
-# def test_list_collections():
-#
-#
-#
-#     assertEqual(200, self.response.status_code)
-#
-#
-#     assert 1 == 1
