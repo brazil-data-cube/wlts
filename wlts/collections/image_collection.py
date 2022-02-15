95c029f83d99b53e84c9cb6f60939de853996421
Fabiana Zioti <fabiana_zioti@hotmail.com>
2022-02-15 16:18:07 -0300

    adding title information in metadata :card_index:

--- wlts/collections/image_collection.py
+++ wlts/collections/image_collection.py
@@ -19,6 +19,7 @@ class ImageCollection(Collection):
             collections_info (dict): The collection information.
         """
         super().__init__(collections_info["name"],
+                         collections_info["title"],
                          collections_info["authority_name"],
                          collections_info["description"],
                          collections_info["detail"],
