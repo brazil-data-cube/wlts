92e281033ef6d013b191c7a3d095c76c76af4320
Fabiana Zioti <fabiana_zioti@hotmail.com>
2022-03-16 16:05:39 -0300

    updating version and removing dependence owslib :sparkles:

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ install_requires = [
     'shapely>=1.6',
     'jsonschema>=3.2',
     'pyproj>=2',
-    'OWSLib==0.21.0',
     'rasterio>=1.1.2,<2',
     'bdc-auth-client @ git+https://github.com/brazil-data-cube/bdc-auth-client@v0.2.3',
 ]
